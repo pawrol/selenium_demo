883cfa68d4c40a4e05e0ab572eaabee607b3780a
pawrol <48085273+pawrol@users.noreply.github.com>
2019-10-18 10:21:11 +0200

    Update login_test.py

--- demo/login_test.py
+++ demo/login_test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def test_log_in():
+def test_log_in_passed():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     driver.get("http://seleniumdemo.com/")
@@ -13,3 +13,12 @@ def test_log_in():
     driver.find_element_by_name("login").click()
     assert driver.find_element_by_link_text("Logout").is_displayed()
 
+def test_log_in_faild():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    driver.get("http://seleniumdemo.com/")
+    driver.find_element_by_xpath("//li[@id = 'menu-item-22']//a").click()
+    driver.find_element_by_id('username').send_keys("kokosz@lp.eu")
+    driver.find_element_by_id('password').send_keys("123123")
+    driver.find_element_by_name("login").click()
+    assert  driver.find_element_by_name("login").is_displayed()
