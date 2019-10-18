d839505b2f8b2d9925a2844faa49fab1f7450819
pawrol <48085273+pawrol@users.noreply.github.com>
2019-10-18 10:10:57 +0200

    Update login_test.py

--- demo/login_test.py
+++ demo/login_test.py
@@ -6,11 +6,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def test_log_in():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
-    wait = WebDriverWait(driver, 10)
     driver.get("http://seleniumdemo.com/")
     driver.find_element_by_xpath("//li[@id = 'menu-item-22']//a").click()
     driver.find_element_by_id('username').send_keys("kokosz@lp.eu")
-    driver.find_element_by_id('password').send_keys("123")
+    driver.find_element_by_id('password').send_keys("kokosz@lp.eu")
     driver.find_element_by_name("login").click()
-
+    assert driver.find_element_by_link_text("Logout").is_displayed()
 
