f8550706ef0a4c38e79e435afb112416e3a2494f
pawrol <48085273+pawrol@users.noreply.github.com>
2019-10-22 12:31:46 +0200

    Add files via upload

---
+++ locators/locators.py
@@ -0,0 +1,31 @@
+
+#class BillingAddress:
+
+ #   register_email = 'reg_email'
+  #  register_pass = 'reg_password'
+   # register_button = "register"
+   # adresses_link = "Addresses"
+    #edit_link = find_element_by_link_text"Edit"
+   # billing_first_name = find_element_by_id("billing_first_name"
+   # billing_last_name = find_element_by_id("billing_last_name")
+   # billing_company_name = find_element_by_id("billing_company")
+   # billing_country = find_element_by_id("billing_country")
+   # billing_addres_1 = find_element_by_id("billing_address_1")
+   # billing_addres_2 = find_element_by_id("billing_address_2")
+   # billing_postcode = find_element_by_id("billing_postcode")
+   # billing_city = find_element_by_id("billing_city")
+   # billing_phone = find_element_by_id("billing_phone")
+   # save_data_button = find_element_by_class_name("button")
+   # assert_text = driver.find_element_by_xpath("//div[@class = 'woocommerce-message']")
+
+
+class MyAccountPage:
+
+    username = 'username'
+    password = 'password'
+    login_button = "login"
+    register_email = 'reg_email'
+    register_password = 'reg_password'
+    register_button = "register"
+    logout_link = "Logout"
+    assert_text_error = "//ul[@class = 'woocommerce-error']//li"
---
+++ pages/my_account_page.py
@@ -0,0 +1,35 @@
+from locators import locators
+
+
+class MyAccountPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = locators.MyAccountPage.username
+        self.password_input = locators.MyAccountPage.password
+        self.login_button = locators.MyAccountPage.login_button
+        self.reg_email_input = locators.MyAccountPage.register_email
+        self.reg_password_input = locators.MyAccountPage.register_password
+        self.reg_button = locators.MyAccountPage.register_button
+        self.log_out_link = locators.MyAccountPage.logout_link
+        #self.addres_link = locators.MyAccountPage.addres_link
+        self.assert_text = locators.MyAccountPage.assert_text_error
+
+    def open_webpage(self):
+        self.driver.get("http://seleniumdemo.com/?page_id=7")
+
+    def login(self, username, password):
+        self.driver.find_element_by_id(self.username_input).send_keys(username)
+        self.driver.find_element_by_id(self.password_input).send_keys(password)
+        self.driver.find_element_by_name(self.login_button).click()
+
+    def create_account(self, email, password):
+        self.driver.find_element_by_id(self.reg_email_input).send_keys(email)
+        self.driver.find_element_by_id(self.reg_password_input).send_keys(password)
+        self.driver.find_element_by_name(self.reg_button).click()
+
+    def logout_displayed(self):
+        return self.driver.find_element_by_link_text(self.log_out_link).is_displayed()
+
+    def error_msg(self):
+       return self.driver.find_element_by_xpath(self.assert_text).text
